d8ee80412855088c991627f91674ffd55f66a948
vseravno <vseravno2019@gmail.com>
2022-10-09 04:40:12 +0700

    feat(): created books component

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -1,5 +1,28 @@
-function About() {
-    return <div>About</div>
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+export default function Books() {
+    const [books, setBooks] = useState([]);
+
+     useEffect(() => {
+        const fetchBooks = async () => {
+            const {data: {results}} = await axios.get('https://gutendex.com/books/')
+            setBooks(results)
+        }
+        fetchBooks();
+    },[])
+
+    return (
+        <ul>
+            {books.map((item:any) => {
+                return (
+                <li key={item.id}>
+                    {item.id}
+                </li>
+                )
+            })}
+        </ul>
+    )
 }
 
-export default About
+

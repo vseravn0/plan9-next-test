ace2289a33c9c70758a857363396f02d20c5f5c7
vseravno <vseravno2019@gmail.com>
2022-10-12 04:33:29 +0700

    feat(): added bookcard component

---
+++ components/ui/BookCard.tsx
@@ -0,0 +1,20 @@
+interface IBookCard {
+    id: number,
+    img: string,
+    title: string,
+    author: string,
+    downloads: number
+    handler: () => {}
+}
+
+export default function BookCard({id,img,title,author,downloads}:IBookCard) {
+
+    return (
+        <div className="p-3 border-2 rounded-md border-black flex flex-col text-center  h-auto w-56" key={id}>
+            <img className="object-cover mb-3" src={img} alt={title}/>
+            <span className="font-bold">{title}</span>
+            <span className="italic">{author}</span>
+            <span className="underline">{downloads}</span>
+        </div>
+    )
+}

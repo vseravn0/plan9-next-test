56b6bab4581a42c46a18932a7893a47fe255ffd2
vseravno <vseravno2019@gmail.com>
2022-10-12 05:52:51 +0700

    feat(): changed book page logic

--- components/ui/BookCard.tsx
+++ components/ui/BookCard.tsx
@@ -4,13 +4,13 @@ interface IBookCard {
     title: string,
     author: string,
     downloads: number | string,
-    handler?: () => {/**/}
+    className?:string
 }
 
-export default function BookCard({id, img, title, author, downloads}: IBookCard) {
+export default function BookCard({id, img, title, author, downloads,className}: IBookCard) {
 
     return (
-        <div className="p-3 border-2 rounded-md border-black flex flex-col text-center  h-auto w-56" key={id}>
+        <div className={`p-3 border-2 rounded-md border-black flex flex-col text-center  h-auto w-56 ${className}`} key={id}>
             <img className="object-cover mb-3" src={img} alt={title}/>
             <span className="font-bold">{title}</span>
             <span className="italic">{author}</span>

906cc235fb92671e632291ed40ee757838dabe55
vseravno <vseravno2019@gmail.com>
2022-10-11 14:54:00 +0700

    feat(): added redirect

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   swcMinify: true,
   env:{
     VERSION:process.env.VERSION
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/books',
+        basePath: false,
+        permanent: false
+      }
+    ]
   }
 }
 
